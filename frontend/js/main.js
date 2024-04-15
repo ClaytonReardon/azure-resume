55d1f59ecbaa5ef5aec4d2168bfd64c7646ddf33
Clayton <plus1059@proton.me>
2024-04-14 17:21:42 -0700

    Created workflows, added Azure Func URL, added resume download function, fixed formatting of Cert section

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,11 +2,11 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = "https://getazureresume.azurewebsites.net/api/GetResumeCounter?code=eW9ypS67fHOtUnmh2xM_pDueZBgANTs-FKGG643_uvC9AzFuTLgMeA==";
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
